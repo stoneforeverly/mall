96e2b3c4fcae7fdd915a98616b66461750d12538
liuyun122333 <397740469@qq.com>
2023-11-18 10:43:25 +0800

    测试类测试验证

--- mall-demo/src/test/java/com/macro/mall/demo/MallDemoApplicationTests.java
+++ mall-demo/src/test/java/com/macro/mall/demo/MallDemoApplicationTests.java
@@ -2,14 +2,16 @@ package com.macro.mall.demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.macro.mall.model.PmsProduct;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 public class MallDemoApplicationTests {
-	private Logger logger = LoggerFactory.getLogger(MallDemoApplicationTests.class);
+//	private Logger logger = LoggerFactory.getLogger(MallDemoApplicationTests.class);
 	@Test
 	public void contextLoads() {
 	}
@@ -21,8 +23,9 @@ public class MallDemoApplicationTests {
 		product.setId(1L);
 		product.setName("小米手机");
 		product.setBrandName("小米");
-		logger.info(mapper.writeValueAsString(product));
-		logger.error(mapper.writeValueAsString(product));
+		log.info(product.toString());
+		log.info(mapper.writeValueAsString(product));
+		log.error(mapper.writeValueAsString(product));
 	}
 
 }
